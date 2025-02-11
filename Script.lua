095aa76e99bd685381a9cb957e445e036f96383b
b4-st <skibidigyate@gmail.com>
2025-02-11 18:55:29 +0100

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,20 @@
+local FakeSpace = "                                                                                                                                            "
+local Event = nil
+if game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents") then
+	if game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents"):FindFirstChild("SayMessageRequest") then
+		Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+	end
+else
+	if game:GetService("TextChatService"):FindFirstChild("TextChannels") then
+		if game:GetService("TextChatService"):FindFirstChild("TextChannels"):FindFirstChild("RBXGeneral") then
+			Event = game:GetService("TextChatService").TextChannels.RBXGeneral
+		end
+	end
+end
+if Event then
+	if Event:IsA("RemoteEvent") then
+		Event:FireServer(_G.RealMessage.. _G.FakeSpace.. _G.FakeMessage, "All")
+	else
+		Event:SendAsync(_G.RealMessage.. _G.FakeSpace.. _G.FakeUsername.. ": ".. _G.FakeMessage, "")
+	end
+end
